153a022934379b91b69a44e8d643f085c55c0a62
alecsandracr <alecsandracr@yahoo.com.br>
2021-09-18 09:55:33 -0300

    Commit inicial

---
+++ Alura.LeilaoOnline.Selenium/Helpers/TestHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace Alura.LeilaoOnline.Selenium.Helpers
+{
+    public static class TestHelper
+    {
+        public static string PastaDoExecutavel => ("D:\\Driver\\chromedriver_win32_83.0.4103.14");
+    }
+}
---
+++ Alura.LeilaoOnline.Selenium/PageObjects/LoginPO.cs
@@ -0,0 +1,66 @@
+﻿using OpenQA.Selenium;
+
+namespace Alura.LeilaoOnline.Selenium.PageObjects
+{
+    public class LoginPO
+    { 
+        private IWebDriver driver; // Primeira armazenou o Webdriver
+        private By byInputLogin; // Localizadores necessarios para page de login
+        private By byInputSenha;
+        private By byBotaoLogin;
+
+        // construtor injeção de dependencia(Meu  Login PO depende de Webdriver)
+        public LoginPO(IWebDriver driver)
+        {
+            this.driver = driver;
+            //localizadores inicializados na criação da classe
+            byInputLogin = By.Id("Login");
+            byInputSenha = By.Id("Password");
+            byBotaoLogin = By.Id("btnLogin");
+        }
+
+        public LoginPO Visitar()
+        {
+            driver.Navigate().GoToUrl("http://localhost:5000/Autenticacao/Login");
+            return this;
+        }
+
+        public LoginPO PreencheFormulario(string login, string senha)
+        {
+            return 
+                InformarEmail(login)
+                .InformarSenha(senha);
+        }
+
+        public LoginPO InformarEmail(string login)
+        {
+            driver.FindElement(byInputLogin).SendKeys(login);
+            return this;
+        }
+
+        public LoginPO InformarSenha(string senha)
+        {
+            driver.FindElement(byInputSenha).SendKeys(senha);
+            return this;
+        }
+
+        public LoginPO EfetuarLogin()
+        {
+            return SubmeteFormulario();
+        }
+
+        public LoginPO SubmeteFormulario()
+        {
+            driver.FindElement(byBotaoLogin).Submit();
+            return this;
+        }
+
+        public void EfetuarLoginComCredenciais(string login, string senha)
+        {
+            Visitar()
+              .PreencheFormulario(login, senha)
+              .SubmeteFormulario();
+        }
+
+    }
+}
